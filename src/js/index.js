1898bfc9813fe2f951710966ade3b2ed4d235552
Dominik Malcharczyk <malcharczyk.dominik@gmail.com>
2020-04-28 19:09:59 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+import number from './test'
+
+export default class App{
+    constructor(){
+
+    }
+    checkIf(){
+        console.log(`to jest główna strona apki i to jest import: ${number}`);
+    }
+}
+
+var xd = new App();
+xd.checkIf();
+
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [ // tutaj plugin kopiuje to co mamy w template - czyli nasz wzór - do folderu .dist jako index.html
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+
+    ]
+}
